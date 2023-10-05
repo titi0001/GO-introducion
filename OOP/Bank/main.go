9a80f9111a57f8ec85a6bc54fc7f86b9d6acd7aa
Thiago Pereira <titiura@gmail.com>
2023-10-05 18:40:06 -0300

    feat: create Interface in go

--- OOP/Bank/main.go
+++ OOP/Bank/main.go
@@ -2,14 +2,27 @@ package main
 
 import (
 	"fmt"
-	// clientes "main/OOP/Bank/clientes"
 	contas "main/OOP/Bank/contas"
 )
+
+func PagarBoleto(conta verificarConta, valorBoleto float64) {
+	conta.Sacar(valorBoleto)
+}
+
+type verificarConta interface {
+	Sacar(valor float64) string
+}
 
 func main() {
 
 	contaDoDenis := contas.ContaPoupanca{}
 	contaDoDenis.Depositar(100)
+	PagarBoleto(&contaDoDenis, 60)
 
+	contaDaLuisa := contas.ContaCorrente{}
+	contaDaLuisa.Depositar(500)
+	PagarBoleto(&contaDaLuisa, 400)
+
+	fmt.Println(contaDaLuisa.ObterSaldo())
 	fmt.Println(contaDoDenis.ObterSaldo())
 }
